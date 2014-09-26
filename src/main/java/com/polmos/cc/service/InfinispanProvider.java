777d53b010d2e8a5872a91d9543f3a7b596868b9
RobicToNieMaKomu <IbetYouWould@know.it>
2014-09-26 21:55:27 +0200

    .

---
+++ src/main/java/com/polmos/cc/service/InfinispanProvider.java
@@ -0,0 +1,27 @@
+package com.polmos.cc.service;
+
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.enterprise.inject.Produces;
+import javax.json.JsonObject;
+
+import org.infinispan.Cache;
+import org.infinispan.manager.CacheContainer;
+import org.jboss.logging.Logger;
+
+@Singleton
+@Startup
+public class InfinispanProvider {
+
+	private static final Logger logger = Logger.getLogger(InfinispanProvider.class);
+	
+	@Resource(lookup = "java:jboss/infinispan/container/mst-cache")
+	private CacheContainer cacheContainer;
+	
+	@Produces
+	public Cache<String, JsonObject> produceCache() {
+		logger.info("producing cache...");
+		return cacheContainer.getCache();
+	}
+}
