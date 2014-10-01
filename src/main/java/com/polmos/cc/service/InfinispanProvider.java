e6cb1249b4e3dd56c4b875207cc36459232e2663
RobicToNieMaKomu <IbetYouWould@know.it>
2014-10-01 19:04:25 +0200

    .

--- src/main/java/com/polmos/cc/service/InfinispanProvider.java
+++ src/main/java/com/polmos/cc/service/InfinispanProvider.java
@@ -1,15 +1,13 @@
 package com.polmos.cc.service;
 
-import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.enterprise.inject.Produces;
 import javax.json.JsonObject;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.infinispan.Cache;
-import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.manager.CacheContainer;
 import org.jboss.logging.Logger;
 
 @Singleton
@@ -18,12 +16,8 @@ public class InfinispanProvider {
 
 	private static final Logger logger = Logger.getLogger(InfinispanProvider.class);
 	
-	private EmbeddedCacheManager cacheManager;
-	
-	@PostConstruct
-	public void startup() throws NamingException {
-		this.cacheManager = (EmbeddedCacheManager) new InitialContext().lookup("java:jboss/infinispan/container/mst-cache-container");
-	}
+	@Resource(lookup = "java:jboss/infinispan/container/myCache")
+	private CacheContainer cacheManager;
 	
 	@Produces 
 	@MSTCache
